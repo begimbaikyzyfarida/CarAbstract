408036ace979472c9558bd69e590cba03748221a
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-14 19:52:46 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Tesla tesla = new Tesla("Tesla S", "White", 250.0);
+        Porsche porsche = new Porsche("Porsche", "Red", 300.0);
+
+        System.out.println("Tesla Details:");
+        System.out.println("Model: " + tesla.getModel());
+        System.out.println("Colour: " + tesla.getColour());
+        System.out.println("Max Speed: " + tesla.getMaxSpeed());
+        System.out.println("Gas: " + tesla.gas());
+        System.out.println("Brake: " + tesla.brake());
+        System.out.println("----------------------------------------");
+        System.out.println("Porsche Details:");
+        System.out.println("Model: " + porsche.getModel());
+        System.out.println("Colour: " + porsche.getColour());
+        System.out.println("Max Speed: " + porsche.getMaxSpeed());
+        System.out.println("Gas: " + porsche.gas());
+        System.out.println("Brake: " + porsche.brake());
+    }
+}
