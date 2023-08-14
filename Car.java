7186c8cc5d5b86820d792bd73c2d9ef4ab4035f6
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-14 19:53:05 +0600

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,24 @@
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public abstract String gas();
+    public abstract String brake();
+
+}
